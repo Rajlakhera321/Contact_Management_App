cbda8937b7a16ffe4e5be78e644b970634f033be
Rajlakhera321 <rajlakhera795@gmail.com>
2025-12-23 08:29:19 +0530

    feat: new changes

--- src/main/java/com/scm/scm20/controllers/UserController.java
+++ src/main/java/com/scm/scm20/controllers/UserController.java
@@ -1,11 +1,20 @@
 package com.scm.scm20.controllers;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.scm.scm20.helper.Helper;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(value = "/dashboard")
     public String userDashboard() {
@@ -14,7 +23,9 @@ public class UserController {
     }
 
     @RequestMapping(value = "/profile")
-    public String userProfile() {
+    public String userProfile(Authentication authentication) {
+        String username = Helper.getEmailOfLoggedInUser(authentication);
+        logger.info("User logged in " + username);
         System.out.println("User Profile requested");
         return "user/profile";
     }
